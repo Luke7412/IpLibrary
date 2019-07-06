562914a216ad4f9df4772c68029d23d594917f8d
Luke7412 <lukasvinkx@hotmail.com>
2019-07-07 00:18:44 +0200

    Cleanup IP

--- lvin_AxiStream_RegisterSlice_v1_0/rtl/RegisterSlice.vhd
+++ lvin_AxiStream_RegisterSlice_v1_0/rtl/RegisterSlice.vhd
@@ -1,128 +1,136 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/20/2018 08:17:36 PM
--- Design Name: 
--- Module Name: RegSlice - rtl
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
+--------------------------------------------------------------------------------
+-- Project Name   : IpLibrary
+-- Design Name    : RegisterSlice
+-- File Name      : RegisterSlice.vhd
+--------------------------------------------------------------------------------
+-- Author         : Lukas Vinkx
 -- Description: 
 -- 
--- Dependencies: 
 -- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 
+--------------------------------------------------------------------------------
+-- LIBRARIES
+--------------------------------------------------------------------------------
 library IEEE;
-    use IEEE.STD_LOGIC_1164.ALL;
+   use IEEE.STD_LOGIC_1164.ALL;
 
-entity RegSlice is
-    Generic(
-        NumByteLanes : natural := 1;
-        UserWidth    : natural := 0;
-        IdWidth      : natural := 0;
-        DestWidth    : natural := 0
-    );
-    Port ( 
-        Glob_AClk        : in  std_logic;
-        Glob_AResetn     : in  std_logic;
-        Target_TValid    : in  std_logic;
-        Target_TReady    : out std_logic;
-        Target_TData     : in  std_logic_vector(8*NumByteLanes-1 downto 0)         := (others => '0');
-        Target_TStrb     : in  std_logic_vector(NumByteLanes-1 downto 0)           := (others => '1');
-        Target_TKeep     : in  std_logic_vector(NumByteLanes-1 downto 0)           := (others => '1');
-        Target_TUser     : in  std_logic_vector(UserWidth*NumByteLanes-1 downto 0) := (others => '0');
-        Target_TId       : in  std_logic_vector(IdWidth-1 downto 0)                := (others => '0');
-        Target_TDest     : in  std_logic_vector(DestWidth-1 downto 0)              := (others => '0');
-        Target_TLast     : in  std_logic                                           := '1';
-        Initiator_TValid : out std_logic;
-        Initiator_TReady : in  std_logic;
-        Initiator_TData  : out std_logic_vector(8*NumByteLanes-1 downto 0);
-        Initiator_TStrb  : out std_logic_vector(NumByteLanes-1 downto 0);
-        Initiator_TKeep  : out std_logic_vector(NumByteLanes-1 downto 0);
-        Initiator_TUser  : out std_logic_vector(UserWidth*NumByteLanes-1 downto 0);
-        Initiator_TId    : out std_logic_vector(IdWidth-1 downto 0);
-        Initiator_TDest  : out std_logic_vector(DestWidth-1 downto 0);
-        Initiator_TLast  : out std_logic
-    );
-end RegSlice;
 
-architecture rtl of RegSlice is
+--------------------------------------------------------------------------------
+-- ENTITY
+--------------------------------------------------------------------------------
+entity RegisterSlice is
+   Generic(
+      g_NumByteLanes : natural := 1;
+      g_UseTKeep     : natural := 1;
+      g_UseTStrb     : natural := 1;
+      g_TUserWidth   : natural := 0;
+      g_TIdWidth     : natural := 0;
+      g_TDestWidth   : natural := 0
+   );
+   Port ( 
+      AClk             : in  std_logic;
+      AResetn          : in  std_logic;
+      Target_TValid    : in  std_logic;
+      Target_TReady    : out std_logic;
+      Target_TData     : in  std_logic_vector(8*g_NumByteLanes-1 downto 0)          := (others => '0');
+      Target_TStrb     : in  std_logic_vector(g_UseTKeep*g_NumByteLanes-1 downto 0) := (others => '1');
+      Target_TKeep     : in  std_logic_vector(g_UseTStrb*g_NumByteLanes-1 downto 0) := (others => '1');
+      Target_TUser     : in  std_logic_vector(g_TUserWidth-1 downto 0)              := (others => '0');
+      Target_TId       : in  std_logic_vector(g_TIdWidth-1 downto 0)                := (others => '0');
+      Target_TDest     : in  std_logic_vector(g_TDestWidth-1 downto 0)              := (others => '0');
+      Target_TLast     : in  std_logic                                              := '1';
+      Initiator_TValid : out std_logic;
+      Initiator_TReady : in  std_logic;
+      Initiator_TData  : out std_logic_vector(8*g_NumByteLanes-1 downto 0);
+      Initiator_TStrb  : out std_logic_vector(g_UseTKeep*g_NumByteLanes-1 downto 0);
+      Initiator_TKeep  : out std_logic_vector(g_UseTStrb*g_NumByteLanes-1 downto 0);
+      Initiator_TUser  : out std_logic_vector(g_TUserWidth-1 downto 0);
+      Initiator_TId    : out std_logic_vector(g_TIdWidth-1 downto 0);
+      Initiator_TDest  : out std_logic_vector(g_TDestWidth-1 downto 0);
+      Initiator_TLast  : out std_logic
+   );
+   begin
+      assert g_UseTKeep = 0 or g_UseTKeep = 1
+         report "g_UseTKeep Must be 0 or 1."
+         severity failure;
+      assert g_UseTStrb = 0 or g_UseTStrb = 1
+         report "g_UseTStrb Must be 0 or 1."
+         severity failure;
+end entity RegisterSlice;
 
-    signal Initiator_TValid_i : std_logic;
-    signal Target_TReady_i    : std_logic;
-    
-    signal Reg_Resetn         : std_logic;
-    signal Reg_TValid         : std_logic;
-    signal Reg_TData          : std_logic_vector(8*NumByteLanes-1 downto 0);
-    signal Reg_TStrb          : std_logic_vector(NumByteLanes-1 downto 0);
-    signal Reg_TKeep          : std_logic_vector(NumByteLanes-1 downto 0);
-    signal Reg_TUser          : std_logic_vector(UserWidth*NumByteLanes-1 downto 0);
-    signal Reg_TId            : std_logic_vector(IdWidth-1 downto 0);
-    signal Reg_TDest          : std_logic_vector(DestWidth-1 downto 0);
-    signal Reg_TLast          : std_logic;
+
+--------------------------------------------------------------------------------
+-- ARCHITECTURE
+--------------------------------------------------------------------------------
+architecture rtl of RegisterSlice is
+
+   signal Initiator_TValid_i : std_logic;
+   signal Target_TReady_i    : std_logic;
+   
+   signal Reg_TValid : std_logic;
+   signal Reg_TData  : std_logic_vector(8*g_NumByteLanes-1 downto 0);
+   signal Reg_TStrb  : std_logic_vector(g_NumByteLanes-1 downto 0);
+   signal Reg_TKeep  : std_logic_vector(g_NumByteLanes-1 downto 0);
+   signal Reg_TUser  : std_logic_vector(g_TUserWidth-1 downto 0);
+   signal Reg_TId    : std_logic_vector(g_TIdWidth-1 downto 0);
+   signal Reg_TDest  : std_logic_vector(g_TDestWidth-1 downto 0);
+   signal Reg_TLast  : std_logic;
 
 begin
 
-    Target_TReady_i <= not Reg_TValid and Reg_Resetn;
+   Target_TReady_i  <= not Reg_TValid;
 
-    Initiator_TValid <= Initiator_TValid_i;
-    Target_TReady    <= Target_TReady_i;
+   Initiator_TValid <= Initiator_TValid_i;
+   Target_TReady    <= Target_TReady_i;
 
 
-    process(Glob_AClk, Glob_AResetn) is
-    begin
+   process(AClk, AResetn) is
+   begin
+      if AResetn = '0' then
+         Initiator_TValid_i <= '0';
+         Reg_TValid         <= '0';
 
-        if Glob_AResetn = '0' then
-            Initiator_TValid_i <= '0';
+      elsif rising_edge(AClk) then
+
+         if Initiator_TValid_i = '1' and Initiator_TReady = '1' then
             Reg_TValid         <= '0';
-            Reg_Resetn         <= '0';
+            Initiator_TValid_i <= Reg_TValid;
+            Initiator_TData    <= Reg_TData;
+            Initiator_TStrb    <= Reg_TStrb;
+            Initiator_TKeep    <= Reg_TKeep;
+            Initiator_TUser    <= Reg_TUser;
+            Initiator_TId      <= Reg_TId  ;
+            Initiator_TDest    <= Reg_TDest;
+            Initiator_TLast    <= Reg_TLast;
+         end if;
 
-        elsif rising_edge(Glob_AClk) then
-            Reg_Resetn <= '1';
-
-            if Initiator_TValid_i = '1' and Initiator_TReady = '1' then
-                Reg_TValid         <= '0';
-                Initiator_TValid_i <= Reg_TValid;
-                Initiator_TData    <= Reg_TData;
-                Initiator_TStrb    <= Reg_TStrb;
-                Initiator_TKeep    <= Reg_TKeep;
-                Initiator_TUser    <= Reg_TUser;
-                Initiator_TId      <= Reg_TId  ;
-                Initiator_TDest    <= Reg_TDest;
-                Initiator_TLast    <= Reg_TLast;
+         if Target_TValid = '1' and Target_TReady_i = '1' then
+            --if (Initiator_TValid_i = '1' and Initiator_TReady = '1') or Initiator_TValid_i = '0' then
+            if Initiator_TReady = '1' or Initiator_TValid_i = '0' then
+               Initiator_TValid_i <= Target_TValid;
+               Initiator_TData    <= Target_TData;
+               Initiator_TStrb    <= Target_TStrb;
+               Initiator_TKeep    <= Target_TKeep;
+               Initiator_TUser    <= Target_TUser;
+               Initiator_TId      <= Target_TId  ;
+               Initiator_TDest    <= Target_TDest;
+               Initiator_TLast    <= Target_TLast;
+            else
+               Reg_TValid <= Target_TValid;
+               Reg_TData  <= Target_TData;
+               Reg_TStrb  <= Target_TStrb;
+               Reg_TKeep  <= Target_TKeep;
+               Reg_TUser  <= Target_TUser;
+               Reg_TId    <= Target_TId  ;
+               Reg_TDest  <= Target_TDest;
+               Reg_TLast  <= Target_TLast;
             end if;
+         end if;
 
-            if Target_TValid = '1' and Target_TReady_i = '1' then
-                if (Initiator_TValid_i = '1' and Initiator_TReady = '1') or Initiator_TValid_i = '0' then
-                    Initiator_TValid_i <= Target_TValid;
-                    Initiator_TData    <= Target_TData;
-                    Initiator_TStrb    <= Target_TStrb;
-                    Initiator_TKeep    <= Target_TKeep;
-                    Initiator_TUser    <= Target_TUser;
-                    Initiator_TId      <= Target_TId  ;
-                    Initiator_TDest    <= Target_TDest;
-                    Initiator_TLast    <= Target_TLast;
-                else
-                    Reg_TValid <= Target_TValid;
-                    Reg_TData  <= Target_TData;
-                    Reg_TStrb  <= Target_TStrb;
-                    Reg_TKeep  <= Target_TKeep;
-                    Reg_TUser  <= Target_TUser;
-                    Reg_TId    <= Target_TId  ;
-                    Reg_TDest  <= Target_TDest;
-                    Reg_TLast  <= Target_TLast;
-                end if;
-            end if;
-
-        end if;
-    end process;
+      end if;
+   end process;
 
 
-end rtl;
+end architecture rtl;
--- lvin_AxiStream_RegisterSlice_v1_0/tb/RegisterSlice_tb.vhd
+++ lvin_AxiStream_RegisterSlice_v1_0/tb/RegisterSlice_tb.vhd
@@ -1,173 +1,207 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 11/20/2018 08:29:01 PM
--- Design Name: 
--- Module Name: RegSlie_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
+--------------------------------------------------------------------------------
+-- Project Name   : IpLibrary
+-- Design Name    : RegisterSlice
+-- File Name      : RegisterSlice_tb.vhd
+--------------------------------------------------------------------------------
+-- Author         : Lukas Vinkx
 -- Description: 
 -- 
--- Dependencies: 
 -- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
+--------------------------------------------------------------------------------
 
 
+--------------------------------------------------------------------------------
+-- LIBRARIES
+--------------------------------------------------------------------------------
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
+   use IEEE.STD_LOGIC_1164.ALL;
+   use IEEE.NUMERIC_STD.ALL;
+   use IEEE.std_logic_arith.all;
 
 
-entity RegSlie_tb is
+--------------------------------------------------------------------------------
+-- ENTITY
+--------------------------------------------------------------------------------
+entity RegisterSlice_tb is
 --  Port ( );
-end RegSlie_tb;
-
-architecture Behavioral of RegSlie_tb is
-
-    component RegSlice is
-        Generic(
-            NumByteLanes : natural := 1;
-            UserWidth    : natural := 0;
-            IdWidth      : natural := 0;
-            DestWidth    : natural := 0
-        );
-        Port ( 
-            Glob_AClk        : in  std_logic;
-            Glob_AResetn     : in  std_logic;
-            Target_TValid    : in  std_logic;
-            Target_TReady    : out std_logic;
-            Target_TData     : in  std_logic_vector(8*NumByteLanes-1 downto 0)         := (others => '0');
-            Target_TStrb     : in  std_logic_vector(NumByteLanes-1 downto 0)           := (others => '1');
-            Target_TKeep     : in  std_logic_vector(NumByteLanes-1 downto 0)           := (others => '1');
-            Target_TUser     : in  std_logic_vector(UserWidth*NumByteLanes-1 downto 0) := (others => '0');
-            Target_TId       : in  std_logic_vector(IdWidth-1 downto 0)                := (others => '0');
-            Target_TDest     : in  std_logic_vector(DestWidth-1 downto 0)              := (others => '0');
-            Target_TLast     : in  std_logic                                           := '1';
-            Initiator_TValid : out std_logic;
-            Initiator_TReady : in  std_logic;
-            Initiator_TData  : out std_logic_vector(8*NumByteLanes-1 downto 0);
-            Initiator_TStrb  : out std_logic_vector(NumByteLanes-1 downto 0);
-            Initiator_TKeep  : out std_logic_vector(NumByteLanes-1 downto 0);
-            Initiator_TUser  : out std_logic_vector(UserWidth*NumByteLanes-1 downto 0);
-            Initiator_TId    : out std_logic_vector(IdWidth-1 downto 0);
-            Initiator_TDest  : out std_logic_vector(DestWidth-1 downto 0);
-            Initiator_TLast  : out std_logic
-        );
-    end component RegSlice;
-
-    constant NumByteLanes : natural := 1;
-    constant UserWidth    : natural := 0;
-    constant IdWidth      : natural := 0;
-    constant DestWidth    : natural := 0;
-
-    signal Glob_AClk        : std_logic;
-    signal Glob_AResetn     : std_logic;
-    signal Target_TValid    : std_logic;
-    signal Target_TReady    : std_logic;
-    signal Target_TData     : std_logic_vector(8*NumByteLanes-1 downto 0)         ;
-    signal Target_TStrb     : std_logic_vector(NumByteLanes-1 downto 0)           ;
-    signal Target_TKeep     : std_logic_vector(NumByteLanes-1 downto 0)           ;
-    signal Target_TUser     : std_logic_vector(UserWidth*NumByteLanes-1 downto 0) ;
-    signal Target_TId       : std_logic_vector(IdWidth-1 downto 0)                ;
-    signal Target_TDest     : std_logic_vector(DestWidth-1 downto 0)              ;
-    signal Target_TLast     : std_logic                                           ;
-    signal Initiator_TValid : std_logic;
-    signal Initiator_TReady : std_logic;
-    signal Initiator_TData  : std_logic_vector(8*NumByteLanes-1 downto 0);
-    signal Initiator_TStrb  : std_logic_vector(NumByteLanes-1 downto 0);
-    signal Initiator_TKeep  : std_logic_vector(NumByteLanes-1 downto 0);
-    signal Initiator_TUser  : std_logic_vector(UserWidth*NumByteLanes-1 downto 0);
-    signal Initiator_TId    : std_logic_vector(IdWidth-1 downto 0);
-    signal Initiator_TDest  : std_logic_vector(DestWidth-1 downto 0);
-    signal Initiator_TLast  : std_logic;
+end entity RegisterSlice_tb;
 
 
-    constant c_PERIOD_200MHZ : time := 5 ns;
-    signal Clk_200MHz : std_logic := '1';
+--------------------------------------------------------------------------------
+-- ARCHITECTURE
+--------------------------------------------------------------------------------
+architecture Behavioral of RegisterSlice_tb is
+
+   constant g_NumByteLanes : natural := 1;
+   constant g_UseTKeep     : natural := 1;
+   constant g_UseTStrb     : natural := 1;
+   constant g_TUserWidth   : natural := 1;
+   constant g_TIdWidth     : natural := 1;
+   constant g_TDestWidth   : natural := 1;
+
+   signal AClk    : std_logic;
+   signal AResetn : std_logic;
+   signal In_TValid, Target_TValid, Initiator_TValid, Out_TValid : std_logic;
+   signal In_TReady, Target_TReady, Initiator_TReady, Out_TReady : std_logic;
+   signal In_TData , Target_TData , Initiator_TData , Out_TData  : std_logic_vector(8*g_NumByteLanes-1 downto 0);
+   signal In_TStrb , Target_TStrb , Initiator_TStrb , Out_TStrb  : std_logic_vector(g_UseTKeep*g_NumByteLanes-1 downto 0);
+   signal In_TKeep , Target_TKeep , Initiator_TKeep , Out_TKeep  : std_logic_vector(g_UseTStrb*g_NumByteLanes-1 downto 0);
+   signal In_TUser , Target_TUser , Initiator_TUser , Out_TUser  : std_logic_vector(g_TUserWidth-1 downto 0);
+   signal In_TId   , Target_TId   , Initiator_TId   , Out_TId    : std_logic_vector(g_TIdWidth-1 downto 0);
+   signal In_TDest , Target_TDest , Initiator_TDest , Out_TDest  : std_logic_vector(g_TDestWidth-1 downto 0);
+   signal In_TLast , Target_TLast , Initiator_TLast , Out_TLast  : std_logic;
+
+   constant c_PeriodAClk : time    := 5 ns;
+   signal ClockEnable    : boolean := False;
+
+   type t_AxisBeat is record
+      TData : std_logic_vector(8*g_NumByteLanes-1 downto 0);
+      TKeep : std_logic_vector(g_UseTKeep*g_NumByteLanes-1 downto 0);
+      TStrb : std_logic_vector(g_UseTStrb*g_NumByteLanes-1 downto 0);
+      TUser : std_logic_vector(g_TUserWidth-1 downto 0);
+      TId   : std_logic_vector(g_TIdWidth-1 downto 0);
+      TDest : std_logic_vector(g_TDestWidth-1 downto 0);
+      TLast : std_logic;
+   end record t_AxisBeat;
 
 begin
 
-    Clk_200MHz <= not Clk_200MHz after c_PERIOD_200MHZ/2;
-    Glob_AClk  <= Clk_200MHz;
+   DUT : entity work.RegisterSlice(rtl)
+      Generic map(
+         g_NumByteLanes => g_NumByteLanes,
+         g_UseTKeep     => g_UseTKeep,
+         g_UseTStrb     => g_UseTStrb,         
+         g_TUserWidth   => g_TUserWidth,
+         g_TIdWidth     => g_TIdWidth,
+         g_TDestWidth   => g_TDestWidth
+      )
+      Port map( 
+         AClk             => AClk,
+         AResetn          => AResetn,
+         Target_TValid    => Target_TValid,
+         Target_TReady    => Target_TReady,
+         Target_TData     => Target_TData,
+         Target_TStrb     => Target_TStrb,
+         Target_TKeep     => Target_TKeep,
+         Target_TUser     => Target_TUser,
+         Target_TId       => Target_TId,
+         Target_TDest     => Target_TDest,
+         Target_TLast     => Target_TLast,
+         Initiator_TValid => Initiator_TValid,
+         Initiator_TReady => Initiator_TReady,
+         Initiator_TData  => Initiator_TData,
+         Initiator_TStrb  => Initiator_TStrb,
+         Initiator_TKeep  => Initiator_TKeep,
+         Initiator_TUser  => Initiator_TUser,
+         Initiator_TId    => Initiator_TId,
+         Initiator_TDest  => Initiator_TDest,
+         Initiator_TLast  => Initiator_TLast 
+      );
 
 
-    DUT : RegSlice
-        Port map( 
-            Glob_AClk        => Glob_AClk       ,
-            Glob_AResetn     => Glob_AResetn    ,
-            Target_TValid    => Target_TValid   ,
-            Target_TReady    => Target_TReady   ,
-            Target_TData     => Target_TData    ,
-            Target_TStrb     => Target_TStrb    ,
-            Target_TKeep     => Target_TKeep    ,
-            Target_TUser     => Target_TUser    ,
-            Target_TId       => Target_TId      ,
-            Target_TDest     => Target_TDest    ,
-            Target_TLast     => Target_TLast    ,
-            Initiator_TValid => Initiator_TValid,
-            Initiator_TReady => Initiator_TReady,
-            Initiator_TData  => Initiator_TData ,
-            Initiator_TStrb  => Initiator_TStrb ,
-            Initiator_TKeep  => Initiator_TKeep ,
-            Initiator_TUser  => Initiator_TUser ,
-            Initiator_TId    => Initiator_TId   ,
-            Initiator_TDest  => Initiator_TDest ,
-            Initiator_TLast  => Initiator_TLast 
-        );
+   Target_TValid    <= In_TValid;
+   In_TReady        <= Target_TReady;
+   Target_TData     <= In_TData;
+   Target_TStrb     <= In_TStrb;
+   Target_TKeep     <= In_TKeep;
+   Target_TUser     <= In_TUser;
+   Target_TId       <= In_TId;
+   Target_TDest     <= In_TDest;
+   Target_TLast     <= In_TLast;
+   
+   Out_TValid       <= Initiator_TValid;
+   Initiator_TReady <= Out_TReady;
+   Out_TData        <= Initiator_TData;
+   Out_TStrb        <= Initiator_TStrb;
+   Out_TKeep        <= Initiator_TKeep;
+   Out_TUser        <= Initiator_TUser;
+   Out_TId          <= Initiator_TId;
+   Out_TDest        <= Initiator_TDest;
+   Out_TLast        <= Initiator_TLast;
 
 
-    process(Glob_AResetn, Glob_AClk) is
-        variable cnt : unsigned(7 downto 0);
-    begin
-        if Glob_AResetn = '0' then
-            Target_TValid <= '0';
-            cnt           := (others => '0');
+   -----------------------------------------------------------------------------
+   p_AClk : process
+   begin
+      AClk <= '0';
+      wait until ClockEnable = True;
+      while ClockEnable loop
+         AClk <= not AClk;
+         wait for c_PeriodAClk/2;
+      end loop;
+      wait;
+   end process;
 
-        elsif rising_edge(Glob_AClk) then
-            if (Target_TValid = '1' and Target_TReady = '1') or Target_TValid = '0' then
-                Target_TValid <= '1';
-                Target_TData  <= std_logic_vector(cnt);
-                cnt           := cnt + 1;
+
+   -----------------------------------------------------------------------------
+   process
+      procedure WaitTics(
+         constant NofTics : natural
+      ) is
+      begin
+         for I in 0 to NofTics-1 loop
+            wait until rising_edge(AClk);
+         end loop;
+      end procedure;
+
+      procedure SendBeat(
+         TData : std_logic_vector;
+         TKeep : std_logic_vector; 
+         TStrb : std_logic_vector;
+         TUser : std_logic_vector;
+         TId   : std_logic_vector;
+         TDest : std_logic_vector;
+         TLast : std_logic
+      ) is
+      begin
+         In_TValid <= '1';
+         In_TData  <= ext(TData, In_TData'length);
+         In_TKeep  <= ext(TKeep, In_TKeep'length); 
+         In_TStrb  <= ext(TStrb, In_TStrb'length);
+         In_TUser  <= ext(TUser, In_TUser'length);
+         In_TId    <= ext(TId, In_TId  'length);
+         In_TDest  <= ext(TDest, In_TDest'length);
+         In_TLast  <= TLast;
+
+         L1 : loop
+            wait until rising_edge(AClk);
+            if In_TValid = '1' and In_TReady = '1' then
+               In_TValid <= '0';
+               exit L1;
             end if;
-        end if;
-    end process;
+         end loop;
+      end procedure;
 
+   begin
 
+      AResetn    <= '0';
+      In_TValid  <= '0';
+      Out_TReady <= '0';
 
-    process
-        procedure WaitCycles(
-            constant NofClkCycles : positive
-        ) is
-        begin
-            wait for (NofClkCycles-1)*c_PERIOD_200MHZ;
-            wait until rising_edge(Glob_AClk);
-        end procedure;
+      wait for 10*c_PeriodAClk;
+      ClockEnable <= True;
 
-    begin
+      WaitTics(10);
+      AResetn <= '1';
 
-        Glob_AResetn     <= '0';
-        Initiator_TReady <= '0';
+      WaitTics(10);
+      In_TValid  <= '1';
+      Out_TReady <= '1';
+      --------------------------------------------------------------------------
 
-        WaitCycles(10);
-        Glob_AResetn <= '1';
+      SendBeat(x"01", "1", "1", "0", "1", "0", '0');
+      SendBeat(x"02", "1", "1", "0", "1", "0", '0');
+      SendBeat(x"03", "1", "1", "0", "1", "0", '0');
+      SendBeat(x"04", "1", "1", "0", "1", "0", '1');
 
-        WaitCycles(10);
-        Initiator_TReady <= '1';
-
-
-        WaitCycles(10);
-        Initiator_TReady <= '0';
-        WaitCycles(1);
-        Initiator_TReady <= '1'; 
-
-
-        wait;
-    end process;
+      --------------------------------------------------------------------------
+      WaitTics(10);
+      AResetn <= '0';
+      WaitTics(10);
+      ClockEnable <= False;
+      wait for 10*c_PeriodAClk;
+      report "Simulation Finished";
+      wait;
+   end process;
 
 end Behavioral;
