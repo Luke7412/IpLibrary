14b49d1c7cac11428b00e4f9d9348695b4f56820
Luke7412 <lukasvinkx@hotmail.com>
2019-07-07 00:17:51 +0200

    Changed port names

--- lvin_Interfaces_AxisUart_v1_0/xgui/AxisUart_vv1_0.tcl
+++ lvin_Interfaces_AxisUart_v1_0/xgui/AxisUart_vv1_0.tcl
@@ -5,6 +5,7 @@ proc init_gui { IPINST } {
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
   ipgui::add_param $IPINST -name "g_AClkFrequency" -parent ${Page_0}
   ipgui::add_param $IPINST -name "g_BaudRate" -parent ${Page_0} -show_range false
+  ipgui::add_param $IPINST -name "g_BaudRateSim" -parent ${Page_0}
 
 
 }
@@ -27,6 +28,15 @@ proc validate_PARAM_VALUE.g_BaudRate { PARAM_VALUE.g_BaudRate } {
 	return true
 }
 
+proc update_PARAM_VALUE.g_BaudRateSim { PARAM_VALUE.g_BaudRateSim } {
+	# Procedure called to update g_BaudRateSim when any of the dependent parameters in the arguments change
+}
+
+proc validate_PARAM_VALUE.g_BaudRateSim { PARAM_VALUE.g_BaudRateSim } {
+	# Procedure called to validate g_BaudRateSim
+	return true
+}
+
 
 proc update_MODELPARAM_VALUE.g_AClkFrequency { MODELPARAM_VALUE.g_AClkFrequency PARAM_VALUE.g_AClkFrequency } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
@@ -36,5 +46,10 @@ proc update_MODELPARAM_VALUE.g_AClkFrequency { MODELPARAM_VALUE.g_AClkFrequency 
 proc update_MODELPARAM_VALUE.g_BaudRate { MODELPARAM_VALUE.g_BaudRate PARAM_VALUE.g_BaudRate } {
 	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
 	set_property value [get_property value ${PARAM_VALUE.g_BaudRate}] ${MODELPARAM_VALUE.g_BaudRate}
+}
+
+proc update_MODELPARAM_VALUE.g_BaudRateSim { MODELPARAM_VALUE.g_BaudRateSim PARAM_VALUE.g_BaudRateSim } {
+	# Procedure called to set VHDL generic/Verilog parameter value(s) based on TCL parameter value
+	set_property value [get_property value ${PARAM_VALUE.g_BaudRateSim}] ${MODELPARAM_VALUE.g_BaudRateSim}
 }
 
