235db4cdcecdce46174d6d04bc7e03d8c6443dd2
Luke7412 <lukasvinkx@hotmail.com>
2022-12-03 01:06:33 +0100

    added axi4s_driver

---
+++ verification__axi4s_driver/src/axi4s_transaction.svh
@@ -0,0 +1,25 @@
+
+
+class Axi4sTransaction #(
+  parameter int TDATA_WIDTH = 8,
+  parameter int TKEEP_WIDTH = TDATA_WIDTH/8,
+  parameter int TSTRB_WIDTH = TDATA_WIDTH/8,
+  parameter int TUSER_WIDTH = 1,
+  parameter int TDEST_WIDTH = 1,
+  parameter int TID_WIDTH   = 1
+);
+
+  logic                   tvalid;
+  logic                   tready;
+  logic [TDATA_WIDTH-1:0] tdata;
+  logic [TKEEP_WIDTH-1:0] tkeep;
+  logic [TSTRB_WIDTH-1:0] tstrb;
+  logic [TUSER_WIDTH-1:0] tuser;
+  logic [TDEST_WIDTH-1:0] tdest;
+  logic [TID_WIDTH]       tid;
+  logic                   tlast;
+
+  
+
+
+endclass
