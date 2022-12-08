3eca9fa78bc0ac15758043b7aa7bb3d6e9f92de1
Luke7412 <lukasvinkx@hotmail.com>
2022-12-08 20:15:23 +0100

    Added headers

--- verification__axi4lite_driver/src/axi4lite_pkg.sv
+++ verification__axi4lite_driver/src/axi4lite_pkg.sv
@@ -1,11 +1,17 @@
+//------------------------------------------------------------------------------
+// Project Name: IpLibrary
+//------------------------------------------------------------------------------
+// Author      : Lukas Vinkx (lvin)
+// Description : 
+//------------------------------------------------------------------------------
 
 
 package axi4lite_pkg;
 
   `include "axi4lite_base.svh"
   `include "Axi4lite_Transaction.svh"
-  // `include "axi4_monitor.svh"
+  `include "axi4lite_monitor.svh"
   `include "axi4lite_master.svh"
-  // `include "axi4_slave.svh"
+  `include "axi4lite_slave.svh"
 
 endpackage
