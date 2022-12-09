504087f7514e97ca1680cec0b00b38ca5dfbf7b6
Luke7412 <lukasvinkx@hotmail.com>
2022-12-10 00:54:06 +0100

    more types

--- packages__types/types_pkg.sv
+++ packages__types/types_pkg.sv
@@ -5,9 +5,11 @@ package types_pkg;
   typedef logic [ 7:0] u8;
   typedef logic [15:0] u16;
   typedef logic [31:0] u32;
+  typedef logic [31:0] u64;
 
   typedef signed logic [ 7:0] i8;
   typedef signed logic [15:0] i16;
   typedef signed logic [31:0] i32;
+  typedef signed logic [31:0] i64;
 
 endpackage
